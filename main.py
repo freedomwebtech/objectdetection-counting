eca0ad78ade6eab0292e23473f5f08993b3f33f9
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-01-28 09:30:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,6 @@ while True:
         continue
  
     frame=cv2.resize(frame,(1020,600))
-    bbox, label, conf = cv.detect_common_objects(frame)
-    frame = draw_bbox(frame, bbox, label, conf)
-    c=label.count('car')
-    p=label.count('person')
-    cv2.putText(frame,'cars'+str(c),(50,60),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),2)
-    cv2.putText(frame,'persons'+str(p),(55,87),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),2)
-
-       
     cv2.imshow("FRAME",frame)
     cv2.setMouseCallback("FRAME",POINTS)
     if cv2.waitKey(1)&0xFF==27:
